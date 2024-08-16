7fa8c7c8557a553634ccdad424d67f2749c37b2b
mac <mac@macs-MacBook-Air.local>
2024-08-16 12:53:24 +0100

    relocate tests

--- tests/test_user_entry.py
+++ tests/test_user_entry.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock, patch
 from sqlalchemy.orm import Session
 import sys
 import os
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 from models import Customers,CustomerInput
 from services.flight_manager import handle_user_data_entry
 
